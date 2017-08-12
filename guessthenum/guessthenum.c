117206dbbc157723add4ce4ad3f901b254bafcc6
Jack DuPlessis <jduplessis294@gmail.com>
2017-08-12 15:10:41 -0400

    Edit c guessthenum

--- guessthenum/guessthenum.c
+++ guessthenum/guessthenum.c
@@ -1,1 +1,1 @@
-int main(){srand(time(0));int i=0,r=rand()%50;for(;;){scanf("%d",&i);if(i<r)printf(">");if(i>r)printf("<");if(i==r){printf("=\n");break;}}}
+int main(){srand(time(0));int i=0,r=rand()%50;for(;;){scanf("%d",&i);if(i<r)puts("low");if(i>r)puts("high");if(i==r){puts("it");break;}}}
---
+++ unorganized/simplecipher.c
@@ -0,0 +1,5 @@
+int main(){
+  char* a="abcdefghijklmnopqrstuv";
+  char str[100];
+  scanf("%s", i);
+}
