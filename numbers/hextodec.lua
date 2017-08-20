c1e31636745a1b2838add5cc40ff2b363d1c7884
Jack DuPlessis <jduplessis294@gmail.com>
2017-08-20 13:16:09 -0400

    Finish lua hextodec

--- numbers/hextodec.lua
+++ numbers/hextodec.lua
@@ -1,3 +1,1 @@
-values={'a','b','c','d','e','f'}
-n=io.read()
-for
+valus={a=10,b=11,c=12,d=13,e=14,f=15}n=io.read()r=0 for i=1,#n do c=n:sub(i,i)m=valus[c] or tonumber(c)r=r+math.pow(16,#n-i)*m end print(r)
