8c86102d80c8bbd4c5ec58b81d1b6c04f3c0d5fa
Jack DuPlessis <jduplessis294@gmail.com>
2017-08-31 17:36:13 -0400

    Golfed down dectohex

--- numbers/dectohex.c
+++ numbers/dectohex.c
@@ -1,13 +1,14 @@
 #define R (int)pow(16,n)
 main(){
-  int i,n=0;
+  int i,t,n=0;
   scanf("%d",&i);
   while(R<i){
     n++;
   }
   char r[n];
+  t=n;
   while(n>-1){
-    r[sizeof(r)-n]=i/R+48+(i/R>9?7:0);
+    r[t-n]=i/R+48+(i/R>9?7:0);
     i%=R;
     n--;
   }
