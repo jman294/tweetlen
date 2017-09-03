fc3da62bacfed74dc6776ee242b1b2f100d0d133
Jack DuPlessis <jduplessis294@gmail.com>
2017-09-03 19:30:36 -0400

    Finished golfing dectohex :)

--- numbers/dectohex.c
+++ numbers/dectohex.c
@@ -1,14 +1,14 @@
 #define R (int)pow(16,n)
 main(){
-  int i,t,n=0;
-  scanf("%d",&i);
+  int t,n=0;
+  scanf("",&i);
   while(R<i){
     n++;
   }
   char r[n];
   t=n;
   while(n>-1){
-    r[t-n]=i/R+48+7*(R<9);
+    r[t-n]=i/R>9*7+48+i/R;
     i%=R;
     n--;
   }
