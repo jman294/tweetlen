1b03cb57814b84342dded5a822cffdcfd2984b74
Jack DuPlessis <jduplessis294@gmail.com>
2017-09-03 20:52:35 -0400

    Mid process working on tictactoewithtin

---
+++ games/oldtictactoewithwin.c
@@ -0,0 +1,18 @@
+int main(){
+  char b[]="012\n456\n89A";
+  char x[2][8]={0};
+  int i,j,c,t;
+  for(;;){
+    c=!c;
+    puts(b);
+    for(i=0;i<2;i++){
+      if(strchr(x[i],3))exit(0);
+    }
+    scanf("%d",&t);
+    x[c][t%4]++;
+    x[c][(t/4)+3]++;
+    x[c][6]+=t%5==0;
+    x[c][7]+=(t+1)%3==0;
+    b[t]=c+42;
+  }
+}
--- games/tictactoewithwin.c
+++ games/tictactoewithwin.c
@@ -1,18 +1,20 @@
 int main(){
   char b[]="012\n456\n89A";
-  char x[2][8]={0};
+  /*char x[2][8]={0};*/
+  char x[17];
   int i,j,c,t;
   for(;;){
     c=!c;
     puts(b);
-    for(i=0;i<2;i++){
-      if(strchr(x[i],3))exit(0);
-    }
+    /*for(i=0;i<2;i++){*/
+      /*if(strchr(x[i],3))exit(0);*/
+      if(strchr(x,3))break;
+    /*}*/
     scanf("%d",&t);
-    x[c][t%4]++;
-    x[c][(t/4)+3]++;
-    x[c][6]+=t%5==0;
-    x[c][7]+=(t+1)%3==0;
+    x[(t%4)*(c+1)]++;
+    x[((t/4)+3)*(c+1)]++;
+    x[6*(c+1)]+=t%5==0;
+    x[7*(c+1)]+=(t+1)%3==0;
     b[t]=c+42;
   }
 }
