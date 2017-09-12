e5b89f3d9b29d03a01c28f2d6619a29b812dc557
Jack DuPlessis <jduplessis294@gmail.com>
2017-09-11 22:01:26 -0400

    Work on tictactoewithwin

--- games/tictactoewithwin.c
+++ games/tictactoewithwin.c
@@ -1,20 +1,18 @@
-int main(){
+main(){
   char b[]="012\n456\n89A";
-  /*char x[2][8]={0};*/
-  char x[17];
-  int i,j,c,t;
+  char x[2][8]={0};
+  int i,c,t;
   for(;;){
     c=!c;
     puts(b);
-    /*for(i=0;i<2;i++){*/
-      /*if(strchr(x[i],3))exit(0);*/
-      if(strchr(x,3))break;
-    /*}*/
+    for(i=0;i<2;i++){
+      if(strchr(x[i],3))exit(0);
+    }
     scanf("%d",&t);
-    x[(t%4)*(c+1)]++;
-    x[((t/4)+3)*(c+1)]++;
-    x[6*(c+1)]+=t%5==0;
-    x[7*(c+1)]+=(t+1)%3==0;
+    x[c][t%4]++;
+    x[c][(t/4)+3]++;
+    x[c][6]+=t%5^1;
+    x[c][7]+=(t+1)%3^1;
     b[t]=c+42;
   }
 }
