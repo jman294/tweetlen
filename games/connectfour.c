ae2c2bd40fd94467cdf8941bf4fb3db33e569d19
Jack DuPlessis <jduplessis294@gmail.com>
2017-09-17 21:48:35 -0400

    Golfed down connectfour

--- games/connectfour.c
+++ games/connectfour.c
@@ -1,19 +1,14 @@
-#define F for(i=0;i<6;i++){
+#define F for(i=0;i<6;i++)
 main(){
  char x[6][8];
  int t,i,c;
- F
-  strcpy(x[i],".......");
- }
+ F strcpy(x[i],":::::::");
  for(;;){
   c=!c;
-  F
-   puts(x[i]);
-  }
+  F puts(x[i]);
   scanf("%d",&t);
-  F
-   if(x[i+1][t]^46)break;
-  }
+  i=0;
+  while(x[i+1][t]==58)i++;
   x[i][t]=c+42;
  }
 }
